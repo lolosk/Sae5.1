35ef0e23667d93c991c993d8bd7c2ff1ab1dbec1
lolosk <genay.lois54@gmail.com>
2026-01-06 09:02:43 +0100

    docker import fix4

--- apps/frontend/src/pages/Login.jsx
+++ apps/frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { apiLogin, apiRegister } from "apps/frontend/src/api.js";
+import { apiLogin, apiRegister } from "../lib/api";
+
 
 
 export default function Login(){
